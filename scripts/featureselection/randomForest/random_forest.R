0ba7e455a7da13cccd7b632912130b8dab656348
mgalland@sils <mgalland@uva.nl>
2017-04-20 14:38:42 +0200

    now computes mean decrease in accuracy for original and permuted datasets

--- scripts/featureselection/randomForest/random_forest.R
+++ scripts/featureselection/randomForest/random_forest.R
@@ -16,10 +16,12 @@
 # load libraries
 library(optparse)
 library(randomForest)
+library(plyr)
 library(dplyr)
 library(caret)
 library(e1071)
 library(doMC)
+
 
 # parse command line arguments
 option_list = list(
@@ -27,6 +29,7 @@ option_list = list(
   make_option(c("-f", "--features"),type="character",default=NULL,help="features data file",metavar="character"),
   make_option(c("-n", "--ntrees"),type="integer",default=500,help="number of trees to build. By default 500. A sufficiently high number of tree is recommended",metavar = "number"),
   make_option(c("-o", "--outdir"), type="character", default="./RF.results/",help="output directory location (by default: ./RF.results", metavar="character"),
+  make_option(c("-r", "--runs"), type="integer",default=100,help="number of runs for RF on original and permuted datasets",metavar="number"),
   make_option(c("-t","--threads"),type="integer",default=4,help="number of threads to use",metavar = "number")
 ) 
 opt_parser = OptionParser(option_list=option_list);
@@ -38,6 +41,7 @@ opt$phenotype = "~/SURFdrive/trichome_team/11.analysis/metabolite_feature_select
 opt$features = "~/SURFdrive/trichome_team/11.analysis/metabolite_feature_selection/20170208_RF_with_sacha/01.whitefly_terpenoids/terpenoids.txt"
 opt$ntrees = 500
 opt$threads = 4
+opt$runs = 4
 
 #############################
 # Read and format input files
@@ -78,25 +82,71 @@ plot(fit.test$err.rate[,1],type="l",col="red",xlab = "Number of trees",ylab = "O
 model = train(class ~ .,data=df,method="rf")
 number_of_variables_to_use = model$bestTune$mtry
 
-####################################################
-# run Random Forest analysis with optimal parameters
-####################################################
-fit.optimized = randomForest(
-  formula = class ~ .,
-  data=df,
-  ntree=opt$ntrees,
-  mtry=number_of_variables_to_use,
-  importance=T)
-  
-#################################
-### Plot most important variables
-#################################
-varImpPlot(fit.optimized)
 
-#################################################################
-# Comparison of OOB errors between original and permuted datasets 
-#################################################################
-# Features p-values 
-# Post-analysis quality check (permutation)
+###################################################################
+# run Random Forest analysis with optimal parameters multiple times
+###################################################################
+fits.original = list()
+for (i in 1:opt$runs){
+  fits.original[[i]] = randomForest(
+    formula = class ~ .,
+    data=df,
+    ntree=opt$ntrees,
+    mtry=number_of_variables_to_use,
+    importance=T)
+}
+
+# original Out Of Bag error (based on confusion matrix)
+original.OOBs = numeric(length=opt$runs)
+for (i in 1:opt$runs){
+  original.OOBs[i] = mean(fits.original[[i]]$err.rate[,1])
+}
+
+# original Mean Decrease in Accuracy for each feature
+original.mdas = list()
+for (i in 1:opt$runs){
+  original.mdas[[i]] = as.data.frame(fits.original[[i]]$importance[,3])
+  colnames(original.mdas[[i]])=paste("mda",as.character(i),sep = "")
+}
+original.mdas = do.call(cbind,original.mdas)
+
+
+#########################################################################################
+# Permutations: comparison of OOB error and MDA values for original and permuted datasets 
+#########################################################################################
+nFeatures = ncol(df) - 1
+
+# helper function to permute the dataframe on one column
+permute.dataframe <- function(df,col2permute){
+  permutedRows = shuffle(nrow(df))
+  df[,col2permute] = df[permutedRows,col2permute]
+  return(df)
+}
+
+# the i loop get MDA values for one feature for the number of requested runs
+# the k loop iterates over the number of features
+# the results are stored in a list
+permuted.mdas = list(length=nFeatures)
+for (k in 1:nFeatures){
+  feature.mdas = numeric(length=opt$runs)
+  for (i in 1:opt$runs){
+    # permute original dataframe for feature k (add +1 to start after the class column)
+    permuted.df = permute.dataframe(df,k+1)
+    # fit a Random Forest on the permuted dataframe
+    fit = randomForest(formula = class ~ .,data = permuted.df,ntree=opt$ntrees,mtry=number_of_variables_to_use,importance=T)
+    # extract Mean Decrease in Accuracy for feature k
+    imp = fit$importance[k,3]
+    feature.mdas[i] = imp                     
+  }
+  permuted.mdas[[k]]=feature.mdas
+}
+# add names of features
+names(permuted.mdas) = colnames(df)[1:nFeatures+1]
+
+# transform into a dataframe
+permuted.res = ldply(permuted.mdas,.id = "feature")
+colnames(permuted.res)=c("feature",paste("mda",seq(1:opt$runs),sep = ""))
+
+
 
 
