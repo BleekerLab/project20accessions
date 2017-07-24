76abbd66b515498b86fc7841949ea9543b2efcba
mgalland <m.galland@uva.nl>
2017-07-24 11:30:40 +0200

    User can make plots and change the control condition

--- scripts/shinyApps/thrips_survival/server.R
+++ scripts/shinyApps/thrips_survival/server.R
@@ -7,6 +7,9 @@
 #    http://shiny.rstudio.com/
 #
 
+##############
+# Load library
+##############
 library(shiny)
 library(optparse)
 library(dplyr,quietly = TRUE,warn.conflicts = F,verbose = F)
@@ -14,69 +17,80 @@ library(survminer)
 library(survival)
 library(ggplot2,verbose = F,warn.conflicts = F)
 
-
 # Define server logic required to draw a histogram
 shinyServer(
   function(input, output) {
-  ############# load data ###############################
+    ############# load data ###############################
     #This function is responsible for loading in the selected file
     filedata <- reactive({
+      
+      # load an example file
       if(input$source==1){
         df <- read.delim("data/example1_dose.txt",header = T,stringsAsFactors = F)
       }
-      else if(input$source==2){
+      
+      # load a user defined file
+      else if (input$source==2){
         infile <- input$upload
         if (is.null(infile)) {
-        # User has not uploaded a file yet
-        return(NULL)
+          # User has not uploaded a file yet
+          return(NULL)
+          }
+        if (is.null(input$control)){
+          # wait until the user has provided a file
+          return(NULL)
         }
-      read.csv(infile$datapath,header=input$header,sep=input$sep)
+        else {
+          df <- read.csv(infile$datapath,header=input$header,sep=input$sep,col.names = c("dose","time","status"),stringsAsFactors = F,check.names = F)
+        }
       }
     })
 
     #This previews the data file
-    output$contents <- renderTable({
+    output$contents <- renderDataTable({
       filedata()
     })
     
-    
-
-    ######### make a survival object #################
-    
-    # example with dose assay
+    ####################################
+    ## Make a survival object and plot
+    ####################################
     observe({
-      if(input$source==1){
-        fit <- with(filedata(),survfit(formula = Surv(time,status) ~ dose,se.fit=T))
+      # Example file
+      if (input$source==1){
+        df <- filedata()
+        df$dose = as.factor(df$dose)
+        levels(df$dose)=c("water","mock","1ug","10ug","100ug")
+        fit <- with(df,survfit(formula = Surv(time,status) ~ dose,se.fit=T))
         output$plot <- renderPlot({
-          gg <- ggsurvplot(fit,conf.int=TRUE,data = filedata())
+          gg <- ggsurvplot(fit,conf.int=TRUE,data = df)
+          gg <- gg$plot + theme_bw() + facet_wrap(~strata)
+          print(gg) 
+      })
+      }
+      # User file
+      if (input$source==2){
+        req(filedata())
+        df <- filedata()
+        df$dose = as.factor(df$dose)
+        # this is how the levels were at the beggining
+        levs = levels(df$dose)
+        output$levels <- renderPrint({levs})
+        # re-order the levels so that the selected control condition is at the beggining 
+        control.cond <- renderText(input$control)
+        reordered.levs = c(input$control,setdiff(levs,input$control))
+        output$newlevs = renderPrint({reordered.levs})
+        levels(df$dose)=reordered.levs
+        # creates a survival object
+        fit <- with(df,survfit(formula = Surv(time,status) ~ dose,se.fit=T))
+        # plot it
+        output$plot <- renderPlot({
+          gg <- ggsurvplot(fit,conf.int=TRUE,data = df)
           gg <- gg$plot + theme_bw() + facet_wrap(~strata)
           print(gg)
-        })
+          })
         }
       })
-    # if(input$source==1){
-    #   fit <- with(filedata,survfit(formula = Surv(time,status) ~ dose,se.fit=T))
-    #   output$plot <- renderPlot({
-    #     gg <- ggsurvplot(fit,conf.int=TRUE,data = dataM)
-    #     gg <- gg$plot + theme_bw() + facet_wrap(~strata)
-    #     print(gg)
-    #     })
-    #   }
 
-    
-    ######### plots ############
-    # output$plot <- renderPlot({
-    #   gg <- ggsurvplot(fit,conf.int=TRUE,data = dataM)
-    #   gg <- gg$plot + theme_bw() + facet_wrap(~strata)
-    #   print(gg)
-    # })
-    
-  
-  #colnames(data) = c("factor","time","status")
-  
-  ######## Kaplan-Meier survival curves ###########
-  # import helper function
-  #source("helpers.R")
 
 })
 
