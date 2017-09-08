cddd49eb2adf67ca9ae0c5332885889fd648bdbb
mgalland <m.galland@uva.nl>
2017-09-08 16:37:29 +0200

    User interface ok for one gene or multiple genes

--- scripts/shinyApps/rnaseqVis/server.R
+++ scripts/shinyApps/rnaseqVis/server.R
@@ -4,25 +4,46 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(data.table)
 
 # load data
-df <- read.delim("data/kallisto_normalized_counts.txt")
+df <- fread("data/kallisto_normalized_counts.txt",sep = "\t",header = T)
 
 # what is your gene of interest? Filter that gene only
 
 shinyServer(function(input,output){
+  # One gene
+  observe({
+    if(input$source==1){
+      filtered.df = dplyr::filter(df,gene == input$gene)
+      }
+    # Multiple genes
+    if(input$source==2){
+      if(is.null(input$mygenes)) {return(NULL)} # no data yet
+      mygenes = input$mygenes
+      filtered.df = dplyr::filter(df,df[,"gene"] %in% mygenes)
+      }  
+  #This previews the data file
+    output$contents <- renderDataTable({
+      filtered.df
+      })
   
   # plot
-  output$genePlot <- renderPlot({
-    # filter df
-    filtered.df = dplyr::filter(df,gene == input$gene)
-    
-    # plot
-    ggplot(data = filtered.df,aes(x = accession,y=value)) +
-      geom_boxplot() +
-      ggtitle(input$gene) +
-      labs(x = "Accession",y = "Normalized expression (counts)") +
-      theme(axis.text.x = element_text(angle=40,hjust=1))
+  observe({
+    #If only one gene to plot
+    if(input$source==1){
+      output$genePlot <- renderPlot({
+        ggplot(data = filtered.df,aes(x = accession,y=value)) +
+          geom_boxplot() +
+          ggtitle(input$gene) +
+          labs(x = "Accession",y = "Normalized expression (counts)") +
+          theme(axis.text.x = element_text(angle=40,hjust=1))
+      })
+    }
   })
+  #   if(input$source==2){
+  #     
+  #   }
+    })
 })
 
--- scripts/shinyApps/rnaseqVis/ui.R
+++ scripts/shinyApps/rnaseqVis/ui.R
@@ -10,12 +10,26 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      textInput("gene",label= h4("Enter your favorite gene here"),value = "Solyc01g008670")
-    ),
+      h3("What do you prefer? One gene or multiple genes?"),
+      radioButtons("source","",list("One gene"=1,"Multiple genes"=2),selected = 1),
+      br(),
+      
+      conditionalPanel(
+        condition="input.source == '1'","This loads one plot for one gene",
+        textInput("gene",label= h4("Enter your favorite gene here"),value = "Solyc01g008670")),
+      
+      conditionalPanel(
+        condition="input.source=='2'",
+        h3("Paste a list of Solyc numbers (Solyc01g008650) separated by commas"),
+        textInput("mygenes",label = "My genes of interest",value = "Solyc01g008650,Solyc01g008670",width='400px'),
+        actionButton('clearText_button','Clear data')
+    )),
     
-    # Show a plot of the generated distribution
+    # Show plots and data
     mainPanel(
-      plotOutput("genePlot")
+      tabsetPanel(
+        tabPanel("Plots",plotOutput("genePlot")),
+        tabPanel("Data input",dataTableOutput("contents"))
     )
   )
-))
+)))
